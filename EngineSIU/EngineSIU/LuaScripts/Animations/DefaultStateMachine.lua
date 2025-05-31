4f1943d0fad289f9867a7325bc62fd276bb90495
wannaBpark <leehun456@hanmail.net>
2025-06-01 05:25:43 +0900

    Added - Bowser Animation 추가 - 착지, 스핀, 맞음, 죽음
            self.current = "Contents/Bowser/Armature|Bowser_Backhit" -- die
            self.current = "Contents/Bowser/Armature|Bowser_Land"
            self.current = "Contents/Bowser/Armature|Bowser_Hit3"
            self.current = "Contents/Bowser/Armature|Bowser_Spin"

--- EngineSIU/EngineSIU/LuaScripts/Animations/DefaultStateMachine.lua
+++ EngineSIU/EngineSIU/LuaScripts/Animations/DefaultStateMachine.lua
@@ -1,12 +1,18 @@
 AnimFSM = {
     current = "Idle",
-    
     Update = function(self, dt)
         -- self.current = "Contents/Fbx/Capoeira.fbx"
 
-        self.current = "Contents/Human/NarutoRun"
-        self.current = "Contents/Bowser/Bowser/Armature.001|bowser_landing"
-        self.current = "Contents/Player_3TTook/Armature|Head_Hit"
+        -- self.current = "Contents/Bowser/Armature.003|Bowser_Landing"     
+        --self.current = "Contents/Bowser/Armature.003|Bowser_Spin"
+        -- self.current = "Contents/Bowser/Armature.002|Bowser_Hit"
+        self.current = "Contents/Bowser/Armature|Armature.002|Armature.002|Bowser_Hit"
+        -- self.current = "Contents/Bowser/Armature|Armature.002|Armature.002|Bowser_Hit"
+        self.current = "Contents/Bowser/Armature|Bowser_Backhit" -- die
+        self.current = "Contents/Bowser/Armature|Bowser_Land"
+        self.current = "Contents/Bowser/Armature|Bowser_Hit3"
+        self.current = "Contents/Bowser/Armature|Bowser_Spin"
+        -- self.current = "Contents/Bowser/Armature.002|Bowser_Die"
 
         return {
             anim = self.current,
