a173402b8c184f029ddd9b34d3cb6d42763bdd4c
wannaBpark <leehun456@hanmail.net>
2025-06-01 16:37:07 +0900

    Added - Animation : falling

--- EngineSIU/EngineSIU/LuaScripts/Animations/DefaultStateMachine.lua
+++ EngineSIU/EngineSIU/LuaScripts/Animations/DefaultStateMachine.lua
@@ -9,14 +9,15 @@ AnimFSM = {
         self.current = "Contents/Bowser/Armature|Armature.002|Armature.002|Bowser_Hit"
         -- self.current = "Contents/Bowser/Armature|Armature.002|Armature.002|Bowser_Hit"
         self.current = "Contents/Bowser/Armature|Bowser_Backhit" -- die
+        self.current = "Contents/Bowser/Armature|Bowser_Falling"
         self.current = "Contents/Bowser/Armature|Bowser_Land"
-        self.current = "Contents/Bowser/Armature|Bowser_Hit3"
         self.current = "Contents/Bowser/Armature|Bowser_Spin"
+        self.current = "Contents/Bowser/Armature|Bowser_Hit3"
         -- self.current = "Contents/Bowser/Armature.002|Bowser_Die"
 
         return {
             anim = self.current,
-            blend = 1.0
+            blend = 0.5
         }
     end
 }
